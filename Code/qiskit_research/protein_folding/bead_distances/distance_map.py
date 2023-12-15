acf95c193aa2677defaaa11092ce35ed2083f4a9
TeamMduse <73825210+TeamMduse@users.noreply.github.com>
2023-12-15 17:24:27 +0100

    Add files via upload

---
+++ Code/qiskit_research/protein_folding/bead_distances/distance_map.py
@@ -0,0 +1,142 @@
+# (C) Copyright IBM 2021, 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+"""A class that stores distances between beads of a peptide as qubit operators."""
+from typing import Union, Tuple, DefaultDict, Dict
+import numpy as np
+
+from qiskit.opflow import PauliSumOp, PauliOp, OperatorBase
+
+from .distance_map_builder import DistanceMapBuilder
+from ..peptide.beads.base_bead import BaseBead
+from ..peptide.pauli_ops_builder import _build_full_identity
+from ..peptide.peptide import Peptide
+from ..qubit_utils.qubit_fixing import _fix_qubits
+
+
+class DistanceMap:
+    """Stores distances between beads of a peptide as qubit operators."""
+
+    def __init__(self, peptide: Peptide):
+        """
+        Args:
+            peptide: A Peptide object that includes all information about a protein.
+        """
+        self._peptide = peptide
+        self._distance_map, self._num_distances, = DistanceMapBuilder().create_distance_qubits(peptide)
+
+    def __getitem__(self, position: Tuple[BaseBead, BaseBead]) -> OperatorBase:
+        item1, item2 = position
+        return self._distance_map[item1][item2]
+
+    @property
+    def peptide(self) -> Peptide:
+        """Returns a peptide."""
+        return self._peptide
+
+    @property
+    def distance_map(self) -> DefaultDict[BaseBead, Dict[BaseBead, OperatorBase]]:
+        """Returns a distance map."""
+        return self._distance_map
+
+    @property
+    def num_distances(self) -> int:
+        """Returns the number of distances calculated."""
+        return self._num_distances
+    
+    def displacement_map(self, axis=0): # this is to get the displacement map, by default along the axis 0
+        """Returns a displacement map between beads in the dimension of one axis"""
+        displacements, num_displacements = DistanceMapBuilder().create_distance_qubits(self.peptide, axis)
+        return displacements
+
+    def first_neighbor(self, peptide: Peptide, lower_bead_ind: int, is_side_chain_lower: int, upper_bead_ind: int, is_side_chain_upper: int,
+        lambda_1: float, pair_energies: np.ndarray, adjustment_mj: bool = False  , energy_mod_mj: [PauliSumOp, PauliOp] = 0 ,  pair_energies_multiplier: float = 0.1 ) -> Union[PauliSumOp, PauliOp]:
+        """
+        Creates first nearest neighbor interaction if beads are in contact
+        and at a distance of 1 unit from each other. Otherwise, a large positive
+        energetic penalty is added. Here, the penalty depends on the neighboring
+        beads of interest (i and j), that is, :math:`lambda_0 > 6*(j -i + 1)*lambda_1 + e_{ij}`.
+        Here, we chose, :math:`lambda_0 = 7*(j- 1 + 1)`.
+
+        Args:
+            *peptide: A Peptide object that includes all information about a protein.
+            *lower_bead_ind: Backbone bead at turn i.
+            *is_side_chain_lower: Side chain on backbone bead i.
+            *upper_bead_ind: Backbone bead at turn j (j > i).
+            *is_side_chain_upper: Side chain on backbone bead j.
+            *lambda_1: Constraint to penalize local overlap between beads within a nearest neighbor contact.
+            *pair_energies: Numpy array of pair energies for amino acids.
+            *pair_energies_multiplier: A constant that multiplies pair energy contributions.
+            *adjustment_mj: It tells us which equation to choose if due to the modification of the Miyazawa - Jerningan potential
+            *energy_mod_mj: Modified energy parameter
+        Returns:
+            Contribution to an energetic Hamiltonian.
+        """
+        bounding_constant = 7
+        lower_bead = peptide.get_main_chain[lower_bead_ind - 1]
+        upper_bead = peptide.get_main_chain[upper_bead_ind - 1]   
+             
+        lambda_0 = bounding_constant * (upper_bead_ind - lower_bead_ind + 1) * lambda_1
+        if is_side_chain_lower == 1: lower_bead = lower_bead.side_chain[0]
+        if is_side_chain_upper == 1: upper_bead = upper_bead.side_chain[0]
+        energy = pair_energies[lower_bead_ind][is_side_chain_upper][upper_bead_ind][is_side_chain_lower]
+        x = self.distance_map[lower_bead][upper_bead]
+        
+        # choose if use the modification of the Miyazawa - Jerningan potential or the original one
+        if adjustment_mj == False:
+            expression = lambda_0 * (x - _build_full_identity(x.num_qubits)) + pair_energies_multiplier * energy * _build_full_identity(x.num_qubits) 
+        else:
+            if  energy_mod_mj == 0 or None:
+                energy_mod_mj = 0
+                energy_mod_mj =  energy_mod_mj* _build_full_identity(x.num_qubits)
+            expression = lambda_0 * (x - _build_full_identity(x.num_qubits)) + pair_energies_multiplier * (energy * _build_full_identity(x.num_qubits) -  energy_mod_mj )
+
+
+        return _fix_qubits(expression)
+
+    def second_neighbor(self, peptide: Peptide, lower_bead_ind: int, is_side_chain_lower: int, upper_bead_ind: int, is_side_chain_upper: int,
+        lambda_1: float, pair_energies: np.ndarray, adjustment_mj: bool = False,   energy_mod_mj: [PauliSumOp, PauliOp] = 0  ,pair_energies_multiplier: float = 0.1 ) -> Union[PauliSumOp, PauliOp]:
+        
+        """
+        Creates energetic interaction that penalizes local overlap between
+        beads that correspond to a nearest neighbor contact or adds no net
+        interaction (zero) if beads are at a distance of 2 units from each other.
+        Ensure second nearest neighbor does not overlap with reference point.
+
+        Args:
+            peptide: A Peptide object that includes all information about a protein.
+            lower_bead_ind: Backbone bead at turn i.
+            is_side_chain_lower: Side chain on backbone bead i.
+            upper_bead_ind: Backbone bead at turn j (j > i).
+            is_side_chain_upper: Side chain on backbone bead j.
+            lambda_1: Constraint to penalize local overlap between beads within a nearest neighbor contact.
+            pair_energies: Numpy array of pair energies for amino acids.
+            pair_energies_multiplier: A constant that multiplies pair energy contributions.
+
+        Returns:
+            Contribution to an energetic Hamiltonian.
+        """
+
+        energy = pair_energies[lower_bead_ind][is_side_chain_upper][upper_bead_ind][is_side_chain_lower]
+        lower_bead = peptide.get_main_chain[lower_bead_ind - 1]
+        upper_bead = peptide.get_main_chain[upper_bead_ind - 1]
+        if is_side_chain_lower == 1: lower_bead = lower_bead.side_chain[0]
+        if is_side_chain_upper == 1: upper_bead = upper_bead.side_chain[0]
+        x = self.distance_map[lower_bead][upper_bead]
+
+         # choose if use the modification of the Miyazawa - Jerningan potential or the original one
+        if adjustment_mj == False:
+            expression = lambda_1 * (2 * (_build_full_identity(x.num_qubits)) - x) + pair_energies_multiplier * energy * _build_full_identity(x.num_qubits)
+        else: 
+            if  energy_mod_mj == 0 or energy_mod_mj == None:
+                energy_mod_mj = 0
+                energy_mod_mj =  energy_mod_mj* _build_full_identity(x.num_qubits)
+
+            expression = lambda_1 * (2 * (_build_full_identity(x.num_qubits)) - x) + pair_energies_multiplier *( energy * _build_full_identity(x.num_qubits) -  energy_mod_mj)
+        return _fix_qubits(expression)
